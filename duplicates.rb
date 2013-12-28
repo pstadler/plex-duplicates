66f0ff789f5cb3e4bd4b581c2a49881c95e3c8d6
Patrick Stadler <patrick.stadler@gmail.com>
2013-12-28 13:36:08 +0100

    update dependencies. remove monkey patch for plex-ruby

--- duplicates.rb
+++ duplicates.rb
@@ -7,26 +7,6 @@ host, port = (ARGV[0] || '').split(':')
 host ||= 'localhost'
 port ||= 32400
 
-module Plex
-  class Video
-    def initialize(node)
-      @attribute_hash = {}
-      node.attributes.each do |method, val|
-        @attribute_hash[Plex.underscore(method)] = val.value
-        define_singleton_method(Plex.underscore(method).to_sym) do
-          val.value
-        end
-      end
-      # monkey patch @media to support multiple entries
-      @media      = node.search('Media').map    { |m| Plex::Media.new(m)    }
-      @genres     = node.search('Genre').map    { |m| Plex::Genre.new(m)    }
-      @writers    = node.search('Writer').map   { |m| Plex::Writer.new(m)   }
-      @directors  = node.search('Director').map { |m| Plex::Director.new(m) }
-      @roles      = node.search('Role').map     { |m| Plex::Role.new(m)     }
-    end
-  end
-end
-
 def pretty_filesize n
   count = 0
   while  n >= 1024 and count < 4
@@ -60,11 +40,11 @@ server.library.sections.each do |section|
       next
     end
 
-    media = item.media
-    if media.length > 1
+    medias = item.medias
+    if medias.length > 1
       out << "#{item.grandparent_title + ': ' if item.type == 'episode'}#{item.title}\n".white
       last_dir = nil
-      media.each do |m|
+      medias.each do |m|
         m.parts.each do |part|
           dir = File.dirname(part.file)
           name = File.basename(part.file)
